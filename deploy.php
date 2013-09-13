572e93206b8d03ae31898c99f7f6f190b6f67cc2
Vishnu Prem <vishnu@vishnuprem.com>
2013-09-13 12:24:52 +0800

    Updated deploy script

---
+++ deploy.php
@@ -0,0 +1,35 @@
+<?php
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+ 
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+ 
+	$file = 'deploy.log';
+	file_put_contents($file, $output, FILE_APPEND);
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
