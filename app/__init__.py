25777f8c2617bd05a1d58986dda6d128f0a2ed8f
chinab91 <chinab91@gmail.com>
2013-09-21 08:16:32 +0800

    routing without pulling from db

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@54.254.104.240/name_of_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@54.254.104.240/schedulr'
 db = SQLAlchemy(app)
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,7 +1,37 @@
 from app import app
 
-"""url routing home page """
 @app.route('/')
-@app.route('/index')
-def homepage():
-	return 'Hello World!'
+def check_if_logged_in():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in' 
+
+@app.route('/user/<username>')
+def profile(username):
+    return 'User name is %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # abort(401)
+    return 'Post %d' % post_id
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username']=request.form['username']
+        return redirect(url_for('check_if_logged_in'))
+    else:
+        return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+        '''
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    app.logger.debug('logging out')
+    return redirect(url_for('check_if_logged_in'))
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
