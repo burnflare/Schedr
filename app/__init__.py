864921de4b54dcc0fccfa3a329d4afba0c7dc14a
chinab91 <chinab91@gmail.com>
2013-09-21 12:53:19 +0800

    removed phpmyadmin from dev and changed to local db

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@54.254.104.240/schedulr'
+app = Flask(__name__, static_url_path='')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1/schedulr'
 db = SQLAlchemy(app)
 
 from app import views, models
