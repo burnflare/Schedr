8405e3b5a7e621d41572fe2078200878b1db046b
Rtexal Tan <r.rtexal@gmail.com>
2013-09-22 19:55:20 +0800

    Updated assets

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -39,7 +39,7 @@ $("button.TR").click(function() {
 });
 
 $('button.inviteMates').click(function() {
-	$('.form-horizontal').fadeOut().empty().fadeIn().append('<span class="matesEmails"><div class="form-group"><label for="mate1" class="col-lg-2 control-label">Mate #1 email?</label><div class="col-lg-10"><input type="email" class="form-control" id="mate1" placeholder="Enter email.."></div></div></span><div class="addMatesbtn center"><button type="button" class="addMates btn btn-success btn-lg"><b>+</b></button></div>');
+	$('.form-horizontal').fadeOut().empty().fadeIn().append('<span class="matesEmails"><div class="form-group"><label for="mate1" class="col-lg-2 control-label">Member #1 email?</label><div class="col-lg-10"><input type="email" class="form-control" id="mate1" placeholder="Enter email.."></div></div></span><div class="addMatesbtn center"><button type="button" class="addMates btn btn-success btn-lg"><b>+</b></button></div>');
 	$('button.inviteMates').empty().append('<b>Sign in with Google</b>').addClass('googleLogin').removeClass('inviteMates');
 	addMates();
 });
@@ -48,7 +48,7 @@ var matesCount = 1;
 function addMates() {
 $('button.addMates').click(function() {
 	matesCount = ++matesCount;
-	$('.matesEmails').append('<div class="form-group"><label for="mate'+matesCount+'" class="col-lg-2 control-label">Mate #'+matesCount+' email?</label><div class="col-lg-10"><input type="email" class="form-control" id="mate'+matesCount+'" placeholder="Enter email.."></div></div>');
+	$('.matesEmails').append('<div class="form-group"><label for="mate'+matesCount+'" class="col-lg-2 control-label">Member #'+matesCount+' email?</label><div class="col-lg-10"><input type="email" class="form-control" id="mate'+matesCount+'" placeholder="Enter email.."></div></div>');
 });
 }
 
