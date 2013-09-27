97869fd92fbac5208d71ca295573c7f595cb5b94
Vishnu Prem <vishnu@vishnuprem.com>
2013-09-27 14:47:43 +0800

    fixed untracked files

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1/schedulr'
-db = SQLAlchemy(app)
-
-from app import views, models
