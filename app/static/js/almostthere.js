ceabeb55cfc836712c9c30d259bdce5139d0ca9c
Rtexal Tan <r.rtexal@gmail.com>
2013-09-27 18:53:03 +0800

    update almostthere

--- app/static/js/almostthere.js
+++ app/static/js/almostthere.js
@@ -8,7 +8,7 @@ $.ajax({
 		  url: "/admin/"
 		}).done(function(json) {
 			$.each(json.event_list,function(i,el){
-			  $('span.meetings').prepend('<a href="calender.html/?pid='+el.meetings_id+'"><button type="button" class="btn btn-success btn-lg btnwide">'+el.event_name+'</button></a>');
+			  $('span.meetings').prepend('<a href="calender.html?pid='+el.meetings_id+'"><button type="button" class="btn btn-success btn-lg btnwide">'+el.event_name+'</button></a>');
 			});	
 	});
 
