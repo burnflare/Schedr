20220baf1dcce16cecdef9abd2f39b5b9355cd04
chinab91 <chinab91@gmail.com>
2013-09-27 14:34:31 +0800

    /login/ works

--- app/views.py
+++ app/views.py
@@ -8,25 +8,26 @@ from models import User, Meetings
 def index():
     return redirect(url_for('static', filename='index.html'))
 
-@app.route('/login/')
+@app.route('/login/', methods=['POST'])
 def login():
-    is_successful, f_name, l_name, email, oAuth_access, oAuth_token = call_google_oauth()
-    if is_successful == 1:
-        existingUsers = User.query.filter_by(email = email).first()
-        if existingUsers is None:
-            newUser = User(f_name, l_name, email, oAuth_access, oAuth_token)
-            db.session.add(newUser)
-            db.session.commit()
-            session['creator_id'] = newUser.user_id
-            return jsonify(test = newUser.user_id)
-        else:
-            session['creator_id'] = existingUsers.user_id    
-            return 'user already exists'
-    return 'not successful'
+    #is_successful, f_name, l_name, email = call_google_oauth()
+    google_ouath_dict = json.loads(request.data)
+    f_name = google_ouath_dict['fname']
+    l_name = google_ouath_dict['lname']
+    email = google_ouath_dict['email']
 
-@app.route('/schedule/<id>', methods=['GET','POST'])
-def manage_user_schedule():
-    return 1
+    #if is_successful == 1:
+    existingUsers = User.query.filter_by(email = email).first()
+    if existingUsers is None:
+        newUser = User(f_name, l_name, email)
+        db.session.add(newUser)
+        db.session.commit()
+        session['creator_id'] = newUser.user_id
+    else:
+        session['creator_id'] = existingUsers.user_id
+
+    return jsonify(user_id = session['creator_id'])
+
 
 @app.route('/event/', methods=['GET', 'POST'])
 def process_event_details():
@@ -35,45 +36,39 @@ def process_event_details():
     else:
         creator_id = 0
 
-    if request.method == 'POST':
-        event_dict = json.loads(request.data)
+    if creator_id != 0:
+        if request.method == 'POST':
+            event_dict = json.loads(request.data)
 
-        #TODO: use escape
-        event_recipients = ",".join(event_dict['recipients'])
-        event_name = event_dict['name']
-        event_venue = event_dict['venue']
-        suggested_date = ",".join(event_dict['date'])
-        suggested_time = ",".join(event_dict['timeslot'])
-        duration = 0 #TODO: event_dict['duration']
+            #TODO: use escape
+            event_recipients = ",".join(event_dict['recipients'])
+            event_name = event_dict['name']
+            event_venue = event_dict['venue']
+            suggested_date = ",".join(event_dict['date'])
+            suggested_time = ",".join(event_dict['timeslot'])
+            duration = 0 #TODO: event_dict['duration']
 
-        newMeeting = Meetings(creator_id, event_recipients, event_name, event_venue, suggested_date, suggested_time, duration)
-        db.session.add(newMeeting)
-        db.session.commit()
-        return 'Post %r' % 'meeting added successfully'
+            newMeeting = Meetings(creator_id, event_recipients, event_name, event_venue, suggested_date, suggested_time, duration)
+            db.session.add(newMeeting)
+            db.session.commit()
+            session['meeting_id'] = newMeeting.meetings_id
+            return 'Post %r' % 'meeting added successfully'
 
-    else:
-        if creator_id != 0:
-            event_details = {}
-            existing_meetings = Meetings.query.filter_by(creator_id = creator_id).all()
-            for i in range(len(existing_meetings)):
-                creator_id = existing_meetings[i].creator_id
-                creator = User.query.filter_by(user_id = creator_id).first()
-                f_name = creator.f_name
-                l_name = creator.l_name
-                full_name = f_name + " " + l_name
-                print full_name
+        else:
+            #if 'meeting_id' in session:
+            if 1:
+                #meeting_id = session['meeting_id']
+                meeting_id = 2
+                current_meeting = Meetings.query.filter_by(meetings_id = meeting_id).first()
+                pprint(current_meeting.meetings_id)
+                current_meeting_dict = row2dict(current_meeting)
+                result = jsonify(current_meeting = current_meeting_dict)
+                session.pop('meeting_id', None)
+                return result
+                
 
-                event_details['meeting_'+str(i)] = row2dict(existing_meetings[i]) + full_name #TODO: think of a way to remove meeting0
-                #event_details = dict(row2dict(existing_meetings[i]).items())
-            #pprint(existing_meetings[0].event_name)
-            pprint(event_details)
-
-            result = jsonify(event_list = event_details)
-            pprint(result)
-            return result
-
-@app.route('/schedule', methods=['GET', 'POST'])
-def schedule():
+@app.route('/schedule/', methods=['GET', 'POST'])
+def manage_user_schedule():
     if request.method == 'POST':
         session['username']=request.form['username']
         return redirect(url_for('check_if_logged_in'))
@@ -101,16 +96,16 @@ def call_google_oauth():
     f_name = 'chinab'
     l_name = 'chugh'
     email = 'chinab91@mail.com'
-    oAuth_access = '1234DQ31R3'
-    oAuth_token = '1DWQ13RQ3R'
-    return is_successful, f_name, l_name, email, oAuth_access, oAuth_token
+    return is_successful, f_name, l_name, email
 
 def row2dict(row):
     d = {}
     for column in row.__table__.columns:
-        if column.name is 'finalized_date':
-            continue        
+        #if column.name is 'finalized_date':
+            #d[column.name] = row.finalized_date.isoformat()        
+            #continue
         d[column.name] = getattr(row, column.name)
+        
     return d
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
