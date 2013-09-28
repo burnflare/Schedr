8c7dfee1a2c01120b8920f70234d1a303371ff57
Vishnu Prem <vishnu@vishnuprem.com>
2013-09-28 20:17:32 +0800

    Removed dashes in gcal's date output

--- app/static/js/google.js
+++ app/static/js/google.js
@@ -64,7 +64,7 @@ function ISODateString(d) {
 	function pad(n) {
 		return n < 10 ? '0' + n : n
 	}
-	return d.getUTCFullYear() + '-' + pad(d.getUTCMonth() + 1) + '-' + pad(d.getUTCDate());
+	return d.getUTCFullYear() + pad(d.getUTCMonth() + 1) + pad(d.getUTCDate());
 }
 
 function dateDifference(dateEarlier, dateLater) {
